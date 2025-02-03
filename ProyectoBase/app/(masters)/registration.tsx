3df5a4bc8d920b27a715c4be2c76d3236419447e
HDVLAP-SOPORTE2\IT HDV <josberhnz@unitec.edu>
2025-02-03 10:55:43 -0600

    Ejercicio 2: Contador con useState y useEffect. 1. Defina un estado contador que se inicialice en 0. 2. Agregue un botón que incremente el contador en 1 cada vez que se presione. 3. Use useEffect para mostrar un mensaje en la consola cada vez que el contador cambie. 4. Si el contador es múltiplo de 5, muestre un mensaje en pantalla: "Ha alcanzado un múltiplo de 5”

--- ProyectoBase/app/(masters)/registration.tsx
+++ ProyectoBase/app/(masters)/registration.tsx
@@ -5,6 +5,13 @@ export default function App() {
   // Ejercicio 1: Manejo de estado con useState
   const [usuario, setUsuario] = useState({ nombre: '', edad: '' });
 
+    // Ejercicio 2: Contador con useState y useEffect
+    const [contador, setContador] = useState(0);
+    useEffect(() => {
+      console.log(`El contador cambió: ${contador}`);
+    }, [contador]);
+  
+
   return (
     <View style={styles.container}>
       {/* Ejercicio 1 */}
@@ -25,6 +32,14 @@ export default function App() {
         }}
       />
       <Text style={styles.message}>Hola, {usuario.nombre}. Tienes {usuario.edad} años.</Text>
+
+      {/* Ejercicio 2 */}
+      <Text style={styles.title}>Contador</Text>
+      <Button title="Incrementar" onPress={() => setContador(contador + 1)} />
+      <Text style={styles.message}>Contador: {contador}</Text>
+      {contador % 5 === 0 && contador !== 0 && <Text style={styles.alert}>Ha alcanzado un múltiplo de 5</Text>}
+
+
 
     </View>
   );
