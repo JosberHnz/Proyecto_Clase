3b70c3da7082793a3b5c7c18960160c087f89764
HDVLAP-SOPORTE2\IT HDV <josberhnz@unitec.edu>
2025-03-05 20:38:33 -0600

    *Se fuerza la renderizacion para asegurar que el componente se actualice cuando cambie el idioma *Ahora se carga el idioma desde AsyncStorage

--- ProyectoBase/app/tabs/settings.tsx
+++ ProyectoBase/app/tabs/settings.tsx
@@ -1,26 +1,34 @@
-import { View, Text, Button, TouchableOpacity } from "react-native";
-import { useRouter } from "expo-router";
-import { useAuth } from "@/contexts/AuthContext";
+import { View, Text, TouchableOpacity } from "react-native";
+import { Picker } from "@react-native-picker/picker";
 import { useTheme } from "@/contexts/ThemeContext";
+import { useLanguage, i18n } from "@/contexts/LanguageContext";
 import { darkTheme, lightTheme } from "@/styles/themes";
+import { useState } from "react";
 
 export default function SettingsScreen() {
     const { theme, toggleTheme } = useTheme();
-    const styles = theme === "dark" ? darkTheme : lightTheme;
-
+    const { language, changeLanguage } = useLanguage();
+    const styles = theme === "dark" ? { ...darkTheme, picker: { width: 200, height: 50, color: "white" } } : { ...lightTheme, picker: { width: 200, height: 50, color: "black" } };
+    
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>
-                Modo Actual: Claro | Oscuro
-            </Text>
-
-            <TouchableOpacity
-                style={styles.button}
-                onPress={toggleTheme}>
-                <Text
-                    style={styles.buttonText}
-                >Cambiar Tema</Text>
+            <Text style={styles.text}>Modo Actual: Claro | Oscuro</Text>
+            <TouchableOpacity style={styles.button} onPress={toggleTheme}>
+                <Text style={styles.buttonText}>Cambiar Tema</Text>
             </TouchableOpacity>
+
+            {/* Selector de idioma */}
+            <Text style={styles.text}>Idioma Actual: {i18n.t("welcome")}</Text>
+            <Picker
+                selectedValue={language}
+                onValueChange={(itemValue) => changeLanguage(itemValue)}
+                style={styles.picker}
+            >
+                <Picker.Item label="Español" value="es" />
+                <Picker.Item label="English" value="en" />
+                <Picker.Item label="Français" value="fr" />
+                <Picker.Item label="Deutsch" value="de" />
+            </Picker>
         </View>
-    )
+    );
 }
